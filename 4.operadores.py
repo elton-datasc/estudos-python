4d500ec868e14fb09bd005db0c1069713217f165
Elton <elton-guilherme@hotmail.com>
2022-09-07 18:58:37 -0300

    operadores lógicos

--- 4.operadores.py
+++ 4.operadores.py
@@ -16,7 +16,7 @@ c = int(3)
 
 print(not a == b or not b < c)
 
-
+"""
 nome = input('Qual seu nome?')
 idade = int(input('Qual sua idade?'))
 
@@ -27,3 +27,22 @@ if idade >= idade_menor and idade <= idade_maior:
   print(f'{nome} pode pegar o empréstimo.')
 else:
   print(f'{nome} NÃO pode pegar o empréstimo.')
+"""
+
+a = 2
+b = 3
+
+if not b > a:
+  print('b é maior do que a')
+else:
+  print('a é maior do que b')
+
+a = ''
+b = 0
+
+if not a:
+  print('Preencha o valor de a')
+
+nome = 'elton'
+if 'l' in nome:
+  print(f'existe l em seu nome')
