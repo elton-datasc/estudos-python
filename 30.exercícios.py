30414149bc9a94212403aa7abde4b69137aed0fc
Elton <elton-guilherme@hotmail.com>
2022-09-17 12:32:28 -0300

    "exercícios"

---
+++ 30.exercícios.py
@@ -0,0 +1,12 @@
+'''n = input('digite um número: ')
+try:
+    if n.isdigit():
+        n = int(n)
+        if n % 2 == 0:
+            print(f'{n} é um número par.')
+        else:
+            print(f'{n} é um número ímpar.')
+    else:
+        print(f'{n} não é número inteiro.')
+except:
+    print('erro.favor reiniciar o programa.')'''
