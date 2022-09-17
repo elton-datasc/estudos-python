41cc5b48c27ee0006d80f7944c3feee55ed3c150
Elton <elton-guilherme@hotmail.com>
2022-09-17 10:42:50 -0300

    "Pass e Ellipsis"

---
+++ 29.Pass e Ellipsis.py
@@ -0,0 +1,15 @@
+valor = False
+
+if valor:
+    #explicando o motivo do pass
+    pass
+else:
+    print('xau')
+
+#ou
+
+if valor:
+    #explicando o que será escrito
+    ...
+else:
+    print('xau')
