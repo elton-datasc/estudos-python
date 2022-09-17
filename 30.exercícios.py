36455f114c034cd55106b7c6058797140552c084
Elton <elton-guilherme@hotmail.com>
2022-09-17 17:37:59 -0300

    "exercícios"

--- 30.exercícios.py
+++ 30.exercícios.py
@@ -10,3 +10,12 @@ try:
         print(f'{n} não é número inteiro.')
 except:
     print('erro.favor reiniciar o programa.')'''
+
+nome = input('digite seu nome: ')
+
+if len(nome) <= 4:
+    print('seu nome é curto')
+elif len(nome) <=6:
+    print('seu nome é normal')
+else:
+    print('seu nome é muito grande')
