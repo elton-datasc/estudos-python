99815231126af3c55858db819d45ded50b994e8d
Elton <elton-guilherme@hotmail.com>
2022-09-18 18:42:16 -0300

    "operações com strings"

---
+++ Strings.py
@@ -0,0 +1,25 @@
+# positivos
+
+texto = 'Python s2'
+
+print(texto[2:4]) # traz th
+
+# negativos
+
+print(texto[-2])
+
+# com steps
+
+print(texto[:8:2]) # fatiando a string de 0 a 7 de 2 em 2
+
+
+# functions built in
+st = 'cada letra do meu texto'
+t1 = st.title()
+t2 = st.upper()
+t3 = st.capitalize()
+#...
+print(t1)
+print(t2)
+print(t3)
+
