7ce0f41490e1d49459290531ba21806fa9eec528
Elton <elton-guilherme@hotmail.com>
2022-09-19 20:55:54 -0300

    "while"

--- 34.While.py
+++ 34.While.py
@@ -1,7 +1,19 @@
 '''while condição verdadeira:
     faça isso'''
 
-x = 0
-while x < 100:
+'''x = 0
+while x < 5:
     print(x)
     x += 1
+print('acabou')'''
+
+x = 0
+while x < 10:
+    if x == 3:
+        x += 1
+        continue
+    print(x)
+    x += 1
+print('Acabou')
+
+
