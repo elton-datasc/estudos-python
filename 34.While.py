eca753b893322d98c4afd1e842416872aa28c75e
Elton <elton-guilherme@hotmail.com>
2022-09-19 20:59:47 -0300

    "while"

--- 34.While.py
+++ 34.While.py
@@ -7,11 +7,20 @@ while x < 5:
     x += 1
 print('acabou')'''
 
-x = 0
+'''x = 0
 while x < 10:
     if x == 3:
         x += 1
         continue
+    print(x)
+    x += 1
+print('Acabou')'''
+
+x = 0
+while x < 10:
+    if x == 3:
+        x += 1
+        break
     print(x)
     x += 1
 print('Acabou')
