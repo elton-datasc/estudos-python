6759d77dfd5041c63cfea43ad515c7a713f7a130
Elton <elton-guilherme@hotmail.com>
2022-10-16 21:46:42 -0300

    "listas"

--- 40.For_Else.py
+++ 40.For_Else.py
@@ -1,7 +1,15 @@
 panelinha = ['elton', 'luciana', 'rayane']
 
-for nome in panelinha:
+'''for nome in panelinha:
     if nome.lower().startswith('l'):
         print('é luuuuuuu')
     elif nome.lower().startswith('r'):
-        print('é rayeeeee')
+        print('é rayeeeee')'''
+
+for nome in panelinha:
+    if nome.lower().startswith('r'):
+        break
+    print(nome)
+else:
+    print('não existe uma palavra que comece com a letra j')
+
