327f034131961fc5f9ea56436dfceff2963f3525
Elton <elton-guilherme@hotmail.com>
2022-10-16 21:27:13 -0300

    "listas"

---
+++ 40.For_Else.py
@@ -0,0 +1,7 @@
+panelinha = ['elton', 'luciana', 'rayane']
+
+for nome in panelinha:
+    if nome.lower().startswith('l'):
+        print('é luuuuuuu')
+    elif nome.lower().startswith('r'):
+        print('é rayeeeee')
