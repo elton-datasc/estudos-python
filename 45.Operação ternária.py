d76ffc64c0bf5a8e51af08f9f131e63967fec245
elton-datasc <elton-guilherme@hotmail.com>
2022-10-18 11:18:00 -0300

    "listas"

---
+++ 45.Operação ternária.py
@@ -0,0 +1,22 @@
+# Operador Ternário
+
+#idade = 15
+msg = 'acesso liberado'
+msgn = 'acesso não liberado'
+
+'''if idade >= 18:
+    print('Acesso liberado')
+else:
+    print('Pode não.')
+
+print(msg) if idade >= 18 else print(msgn)'''
+
+#exemplo de uso
+
+idade = input('Digite sua idade: ')
+
+if not idade.isnumeric():
+    print('Idade não pode ser texto.Digite novamente')
+else:
+    idade = int(idade)
+    print(msg) if idade >= 18 else print(msgn)
