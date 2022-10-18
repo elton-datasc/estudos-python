73ea57ca9d2b0c88c5e6c582e7b0cb5d882bdbbf
elton-datasc <elton-guilherme@hotmail.com>
2022-10-18 15:04:10 -0300

    "listas"

--- 0.Compreensão de lista_Walisson Silva.py
+++ 0.Compreensão de lista_Walisson Silva.py
@@ -1,4 +1,16 @@
-import random
-
 # gerar uma lista com 20 números aleatórios entre 1 e 10
+import random
+random.randint(1, 10)
 
+'''numeros = []
+for _ in range(20):
+    numeros_aleatorios = random.randint(1, 10)
+    numeros.append(numeros_aleatorios)
+print(numeros)'''
+
+#o mesmo que o código de cima, com compreensão de lista
+n= ([random.randint(1, 10) for _ in range(20)])
+print(n)
+
+#trazendo só os pares
+print([i for i in n if i % 2 == 0])
