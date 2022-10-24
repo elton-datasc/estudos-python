2d298cd50953498e55842b2e489940018fd250df
Elton <elton-guilherme@hotmail.com>
2022-10-23 22:53:48 -0300

    "listas"

--- 46.Operador OR com condição.py
+++ 46.Operador OR com condição.py
@@ -1,3 +1,7 @@
 nome = input('Qual o seu nome? ')
 
-print(nome) if nome else print('Você não digitou nada =/')
+#print(nome) if nome else print('Você não digitou nada =/')
+
+#ou
+
+print(nome or 'Você não digitou nada =/')
