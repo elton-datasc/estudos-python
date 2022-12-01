8314b69cf0ac12bf115f42e19306a8821ff736db
elton-datasc <elton-guilherme@hotmail.com>
2022-12-01 17:44:21 -0300

    args

--- 110.args.py
+++ 110.args.py
@@ -9,6 +9,12 @@ def soma(*args):
         print('total', total, numero)
         total = total + numero
         print('total', total)
-
-
 soma(1,2,3,4,5)
+
+def somas(*args):
+    total=0
+    for numero in args:
+        total= numero + total
+    return total
+
+print(somas(3,6,2))
