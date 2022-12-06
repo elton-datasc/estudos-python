a928cbaf3e1e45c04974e92ac3ff8e08058d0a06
elton-datasc <elton-guilherme@hotmail.com>
2022-12-06 10:30:04 -0300

    exercicio

--- 112.exercicios.py
+++ 112.exercicios.py
@@ -8,11 +8,24 @@
 multiplicar = mult(3,7)
 print(multiplicar)'''
 
+# retornando par ou impar
 def par():
     num = int(input('digite um número: '))
     print(f'O número {num} é par') if num % 2 ==0 else print(f'O número {num} é impar')
 
-    
+
 par()
+
+#outra forma de retornar par ou impar
+def par_impar(numero):
+    multiplo_de_dois = numero % 2 == 0
+    if multiplo_de_dois:
+        return f'{numero} é par'
+    return f'{numero} é impar'
+
+print(par_impar(45))
+print(par_impar(56))
+print(par_impar(12))
+print(par_impar(11))
 
 
